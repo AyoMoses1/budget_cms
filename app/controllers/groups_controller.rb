1b63b06aabff144b1c5292b625cc3ba1b23f26f6
Ayo Moses <ayomoses@afrimash.com>
2023-06-08 19:26:26 +0100

    Create icons folder when adding a category image

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,13 +3,16 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = current_user.groups.all.includes(:expenses)
+    @total_expenses = current_user.groups.joins(:expenses).sum('expenses.amount')
   end
 
   def splash; end
 
   # GET /groups/1 or /groups/1.json
   def show
+    @group = Group.find(params[:id])
+    render 'show', locals: { group: @group }
   end
 
   # GET /groups/new
@@ -23,16 +26,19 @@ class GroupsController < ApplicationController
 
   # POST /groups or /groups.json
   def create
-    @group = Group.new(group_params)
+    @group = current_user.groups.new(group_params)
 
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to group_url(@group), notice: "Group was successfully created." }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
+    if @group.save
+      icon_file = params[:group][:icon]
+      if icon_file
+        icon_filename = icon_file.original_filename
+        icon_filepath = Rails.root.join('app', 'assets', 'images', 'icons', icon_filename)
+        File.binwrite(icon_filepath, icon_file.read)
+        @group.update(icon: "icons/#{icon_filename}")
       end
+      redirect_to user_groups_path(current_user), notice: 'Category was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -40,7 +46,7 @@ class GroupsController < ApplicationController
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: "Group was successfully updated." }
+        format.html { redirect_to group_url(@group), notice: 'Category was successfully updated.' }
         format.json { render :show, status: :ok, location: @group }
       else
         format.html { render :edit, status: :unprocessable_entity }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[show edit update destroy]
 
   # GET /users or /users.json
   def index
     @users = User.all
-  end
-
-  # GET /users/1 or /users/1.json
-  def show
   end
 
   # GET /users/new
@@ -15,36 +11,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
-  end
-
-  # POST /users or /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    # Code for editing a user
   end
 
   # DELETE /users/1 or /users/1.json
@@ -52,19 +20,32 @@ class UsersController < ApplicationController
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def show
+    return unless params[:id] == 'sign_out'
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name)
-    end
+    sign_out_and_redirect
+  end
+
+  private
+
+  def sign_out_and_redirect
+    sign_out current_user
+    redirect_to splash_path
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+
+  def set_user
+    @user = current_user
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
